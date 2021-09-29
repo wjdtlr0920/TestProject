141de9e29de1cacccc231deb8dde77e5d8bd44fc
[김정식] <wjdtlr0920@castfox.com>
2021-09-29 12:30:30 +0900

    [김정식] 메인 리싸이클러뷰 달기

--- app/src/main/java/com/js/testproject/MainActivity.kt
+++ app/src/main/java/com/js/testproject/MainActivity.kt
@@ -2,11 +2,22 @@ package com.js.testproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.js.testproject.adapter.MainAdapter
+import com.js.testproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val testList = resources.getStringArray(R.array.test_list)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.rvMain.adapter = MainAdapter(testList)
 
 
     }
