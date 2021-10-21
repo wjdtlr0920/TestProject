c2fc73d7b486705bc10eb4fbe26fe33fbdf66514
wjdtlr0920 <wjdtlr0920@gmail.com>
2021-10-21 18:54:57 +0900

    ㅇㅇ

---
+++ app/src/main/java/com/js/testproject/stack/StackActivity.kt
@@ -0,0 +1,21 @@
+package com.js.testproject.stack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.js.testproject.R
+import com.js.testproject.databinding.ActivityStackBinding
+
+class StackActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityStackBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_stack)
+
+
+
+    }
+}
