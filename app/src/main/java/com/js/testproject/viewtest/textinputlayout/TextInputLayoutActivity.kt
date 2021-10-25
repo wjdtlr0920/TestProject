90eb429dee244684f386c418f86a3c6872ab1354
wjdtlr0920 <wjdtlr0920@gmail.com>
2021-10-25 19:33:01 +0900

    test

--- app/src/main/java/com/js/testproject/viewtest/textinputlayout/TextInputLayoutActivity.kt
+++ app/src/main/java/com/js/testproject/viewtest/textinputlayout/TextInputLayoutActivity.kt
@@ -3,10 +3,17 @@ package com.js.testproject.viewtest.textinputlayout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.js.testproject.R
+import com.js.testproject.databinding.ActivityTextInputLayoutBinding
 
 class TextInputLayoutActivity : AppCompatActivity() {
+
+    private val binding = ActivityTextInputLayoutBinding.inflate(layoutInflater)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_text_input_layout)
+        setContentView(binding.root)
+
+
+
     }
 }
