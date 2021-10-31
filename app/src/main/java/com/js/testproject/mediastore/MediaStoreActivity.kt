c086d5a4ed5c6e465066a69b886e130cfafda78f
DESKTOP-PQMIVLS\J_sik <wjdtlr0920@gamil.com>
2021-10-31 23:00:04 +0900

    포맷 후 테스트

---
+++ app/src/main/java/com/js/testproject/mediastore/MediaStoreActivity.kt
@@ -0,0 +1,12 @@
+package com.js.testproject.mediastore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.js.testproject.R
+
+class MediaStoreActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_media_store)
+  }
+}
