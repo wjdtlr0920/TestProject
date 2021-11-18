7ec4d75bbae2e563e3b7f4e2eba7a8d0814f3021
[김정식] <wjdtlr0920@gmail.com>
2021-11-18 13:11:28 +0900

    [김정식] TextViewActivity 추가

---
+++ app/src/main/java/com/js/testproject/coroutine/CoroutineActivity.kt
@@ -0,0 +1,11 @@
+package com.js.testproject.coroutine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoroutineActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_coroutine)
+  }
+}
